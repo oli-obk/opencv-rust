6e7ab3a3b0712c477f9df137b27b57c1d6557222
Oliver Schneider <git1984941651981@oli-obk.de>
2015-10-22 15:49:38 +0200

    better error reporting in video_capture example

--- examples/video_capture.rs
+++ examples/video_capture.rs
@@ -5,13 +5,16 @@ use opencv::highgui;
 
 fn run() -> Result<(),String> {
     let window = "video capture";
-    try!(highgui::named_window(window,1));
-    let mut cam = try!(highgui::VideoCapture::device(1));
+    try!(highgui::named_window(window, 1));
+    let mut cam = try!(highgui::VideoCapture::device(0));
+    assert!(try!(cam.is_opened()));
     loop {
         let mut frame = try!(core::Mat::new());
-        try!(cam.read(&mut frame));
+        assert!(try!(cam.read(&mut frame)));
         if try!(frame.size()).width > 0 {
             try!(highgui::imshow(window, &mut frame));
+        } else {
+            println!("zero sized frame");
         }
         if try!(highgui::wait_key(10)) > 0 {
             break;
